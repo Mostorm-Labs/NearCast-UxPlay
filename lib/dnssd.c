0fbd376471ceef30029ce6356c31c548c1a3aea6
Chad <luckkievan@gmail.com>
2026-08-13 17:45:41 +0800

    Fix random PIN password discovery flags

--- lib/dnssd.c
+++ lib/dnssd.c
@@ -508,6 +508,7 @@ dnssd_register_raop(dnssd_t *dnssd, unsigned short port)
     dnssd->TXTRecordSetValue(&dnssd->raop_record, "rhd", strlen(RAOP_RHD), RAOP_RHD);
     switch (dnssd->pin_pw) {
     case 2:
+    case 3:
         dnssd->TXTRecordSetValue(&dnssd->raop_record, "pw", strlen("true"), "true");
 	dnssd->TXTRecordSetValue(&dnssd->raop_record, "sf", 4, "0x84");
 	break;
@@ -589,6 +590,7 @@ dnssd_register_airplay(dnssd_t *dnssd, unsigned short port)
 	dnssd->TXTRecordSetValue(&dnssd->airplay_record, "flags", 3, "0x4");
 	break;  
     case 2:  // require password
+    case 3:
         dnssd->TXTRecordSetValue(&dnssd->airplay_record, "pw", strlen("true"), "true");
         dnssd->TXTRecordSetValue(&dnssd->airplay_record, "flags", 3, "0x4");
         break;
